ee98d96408027829e7b1ee3fa006af73bdfbbba6
Satoshi Komatsu <zlia.6.lj.425@gmail.com>
2017-11-18 23:51:20 +0900

    datePicker

--- sample2/secoundViewController.swift
+++ sample2/secoundViewController.swift
@@ -10,15 +10,29 @@ import UIKit
 
 class secoundViewController: UIViewController {
 
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var dateLabel: UILabel!
+    var date: Date!
+    var formatter: DateFormatter!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        date = datePicker.date
+        
+        formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        
+        
     }
 
     @IBAction func back(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
     
-
+    @IBAction func button(_ sender: Any) {
+        let dateText = formatter.string(from: date!)
+        dateLabel.text = dateText
+    }
+    
 }
---
+++ sample2/thirdViewController.swift
@@ -0,0 +1,18 @@
+//
+//  thirdViewController.swift
+//  sample2
+//
+//  Created by Satoshi Komatsu on 2017/11/18.
+//  Copyright © 2017年 Satoshi Komatsu. All rights reserved.
+//
+
+import UIKit
+
+class thirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
